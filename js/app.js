d872a7b1d57050b540808fe1aa1cdf061b3ad9fa
shamim-s <shamim.sarkar379@gmail.com>
2022-09-03 00:58:48 +0600

    Catagories data loaded

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const loadCatagories = async() => {
+    const url = `https://openapi.programming-hero.com/api/news/categories`;
+    try{
+        const res = await fetch(url);
+        const data = await res.json();
+        console.log(data);
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+loadCatagories();
