afd64bfae23d448ea0ce428d870877759a7e3cea
Shamim Sarkar <shamimibas@gmail.com>
2022-09-03 16:33:20 +0600

    Added click to view more details

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// Load all catagory data ======================================================
 const loadCatagoriesData = async() => {
     const url = `https://openapi.programming-hero.com/api/news/categories`;
     try{
@@ -10,12 +11,12 @@ const loadCatagoriesData = async() => {
     }
 }
 
+// Display all catagory =======================================================
 const displayCatagoriesData = (catagories) => {
-    console.log(catagories);
     const catagoriesContainer = document.getElementById('catagories-container');
+    catagoriesContainer.innerHTML = '';
 
     catagories.forEach(catagorie => {
-        console.log(catagorie);
         const catagorieDiv = document.createElement('div');
         catagorieDiv.classList.add('catagorie');
         catagorieDiv.innerHTML = `
@@ -25,10 +26,22 @@ const displayCatagoriesData = (catagories) => {
         catagoriesContainer.appendChild(catagorieDiv);
     });
 }
+// Loading Spinner -------------------------------------------
+const spinner = isLoading => {
+    const loadingSpinner = document.getElementById('spinner');
+    if(isLoading){
+        loadingSpinner.classList.remove('d-none');
+    }else{
+        loadingSpinner.classList.add('d-none');
+    }
+}
 
-const loadNewsData = async(id, name) => {
-    const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
+// Load news data by catagory ==================================================
+const loadNewsData = async(NewsCatagorie, name) => {
+    // start spinner 
+    spinner(true);
 
+    const url = `https://openapi.programming-hero.com/api/news/category/${NewsCatagorie}`;
     try{
         const res = await fetch(url);
         const data = await res.json();
@@ -38,34 +51,104 @@ const loadNewsData = async(id, name) => {
         console.log(error);
     }
 }
-
+// Display all news by catagory=================================================
 const displayNews = (newses, name) => {
     // console.log(newses);
+    const newsContainer = document.getElementById('news-container');
+
+    // set news quntity number each catagory
+    const foundNewsNumber = document.getElementById('found-number');
+    foundNewsNumber.innerText =`${newses.length}`;
+
+    // Display catagory name in items found for category 
     const foundCatagory = document.getElementById('found-catagorie');
     foundCatagory.innerText =` ${name}`;
 
-    const newsContainer = document.getElementById('news-container');
-    const foundNewsNumber = document.getElementById('found-number');
-    foundNewsNumber.innerText =`${newses.length}`;
+    // No News found massage 
+    const notFoundMassage = document.getElementById('no-news-found-massage');
+    if(newses.length === 0){
+        notFoundMassage.classList.remove('d-none');
+    }else{
+        notFoundMassage.classList.add('d-none');
+    }
+
     newsContainer.innerHTML = '';
 
     newses.forEach(news =>{
-        // console.log(news);
         const newsDiv = document.createElement('div');
-        newsDiv.classList.add("row", "gy-5", "gx-6","bg-white", "p-2", "rounded-2", "mt-4");
+        newsDiv.classList.add("row", "gx-6","bg-white", "p-2", "rounded-2", "mt-4");
         newsDiv.innerHTML = `
-    <div class="col-sm-12 col-md-4 h-50">
+    <div class="col-sm-12 col-md-4">
         <img src="${news.image_url}" alt="" class="img-fluid">
     </div>
     <div class="col-sm-12 col-md-8"> 
         <h3>${news.title}</h3>
 
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum quasi officiis sunt expedita eaque distinctio illo animi nemo delectus, consequatur accusamus repellat debitis explicabo pariatur labore asperiores ipsa eos voluptas.</p>
+        <p class="text-truncate">${news.details}</p>
 
-        <div><img src="..." alt=""> <span>Hello</span>Hello<span></span>Hello<span></span></div>
+        <div class="d-flex justify-content-center align-items-center">
+            <div class="w-25 d-flex justify-content-center align-items-center">
+                <img src="${news.author.img}" alt="" class="img-fluid w-25 rounded-5">
+                <div class="">
+                    <h6>${news.author?.name ?? "No data found"}</h6>
+                    <p>${news.author?.published_date ?? "No data found"}</p>
+                </div>
+            </div> 
+            <div class="ms-5 w-75 d-flex justify-content-around align-items-center">
+                <span><i class="fa-regular fa-eye"></i> ${news?.total_view ?? "No data found"}</span>
+                <span>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star-half-stroke"></i>
+                </span>
+                <span>
+                    <i id="more-detail" onclick="loadNewsCardDetails('${news._id}')" class="fa-solid fa-arrow-right fs-2 text-primary" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal"></i>
+                </span>
+                <h6></h6>
+            </div>
+        </div>
     </div>
         `;
         newsContainer.appendChild(newsDiv);
     });
+
+    // stop spinner 
+    spinner(false);
 }
+
+// load news card full details =====================================================
+const loadNewsCardDetails = (news_id) => {
+    const url = `https://openapi.programming-hero.com/api/news/${news_id}`;
+
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayNewsCardDetails(data.data[0]))
+}
+
+// Display news card full details =====================================================
+const displayNewsCardDetails = details => {
+    console.log(details);
+
+    const modalContainer = document.getElementById('modal-dialog');
+    modalContainer.innerHTML = '';
+    const modalContent = document.createElement('div');
+    modalContent.classList.add('modal-content');
+
+    modalContent.innerHTML = `
+    <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">${details.title}</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    </div>
+    <div class="modal-body">
+        <p>${details.details}</p>
+    </div>
+    <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+    </div>
+    `;
+    modalContainer.appendChild(modalContent);
+}
+
 loadCatagoriesData();
